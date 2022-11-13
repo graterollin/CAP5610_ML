c210173026cd15370f0573e0d18f949e4bbe6aa0
graterollin <angels.andres0@gmail.com>
2022-11-12 22:35:06 -0500

    Completed com[ute new centroids

--- hw3/AndresGraterol_HW3.py
+++ hw3/AndresGraterol_HW3.py
@@ -60,22 +60,30 @@ def cosine_similarity():
 def jarcard_similarity():
     return None
 
+def compute_SSE(centers, dataset):
+    return None
+
 # This function computes the centroids and determines 
 # when to stop the algorithm
 # TODO: Keep track of SSE listing here as well
 def kmeans_algorithm(centers, dataset, similarity_measure):
     #new_centers = []
     #centers_history = []
+    SSE_list = []
+
+    iterations = 0
 
     while (1):
-        new_centers = compute_new_centroids(centers, dataset, similarity_measure)
+        new_centers, SSE = compute_new_centroids(centers, dataset, similarity_measure)
 
         # return the final centers once they have converged (they do not change)
         if (new_centers == centers):
-            return centers
+            return centers, iterations, SSE_list
         # Else, keep iterating
         else:
+            SSE_list.append(SSE)
             centers = new_centers
+            iterations += 1
 
         # TODO: What if we return an empty list?
         # return the final centers once they have converged
@@ -91,9 +99,6 @@ def compute_new_centroids(centers, dataset, similarity_measure):
     
     # Create 2D one-hot encoding array for cluster membership
     membership_matrix = [] 
-
-    # TODO: Compute SSE, create function 
-
 
     # For every point in the dataset 
     for point in range(len(dataset)):
@@ -132,9 +137,45 @@ def compute_new_centroids(centers, dataset, similarity_measure):
         membership_array[cluster_member] = 1 
         membership_matrix.append(membership_array)
 
+    # TODO: Compute SSE, create function
+    # Compute SSE for these centers and all the points 
+    SSE = compute_SSE(centers, dataset, membership_matrix)
 
+    # Compute the new centers
+    # Get the total count in each cluster 
+    cluster_totals = [sum(x) for x in zip(*membership_matrix)]
+    # Expect shape: (10,)
+    print("Number per cluster shape:")
+    print(cluster_totals.shape)
+    print("Number per cluster:")
+    print(cluster_totals)
 
-    return None
+    # 10 rows (one for each cluster)
+    # 784 columns 
+    # Shape: (10, 784)
+    # TODO: Check if this is initiated right 
+    sum_per_cluster = np.zeros((10, 784))
+
+    # Get the sum for each cluster
+    # One column of the membership matrix at a time
+    # Fills the sums per cluster row at a time
+    for i in range(10):
+        # represents the rows of the membership matrix 
+        for j in range(len(dataset)):
+            if (membership_matrix[j][i] == 1):
+                # Add to the cluster row    The point in row j  
+                sum_per_cluster[i] = np.add(sum_per_cluster[i], dataset[j])
+
+    new_centers = np.zeros((10, 784))
+
+    # Divide each cluster row by the number of points in each cluster 
+    for i in range(10):
+        new_centers[i] = [x / cluster_totals[i] for x in sum_per_cluster[i]] 
+    
+    print("Shape of new centers:")
+    print(new_centers.shape)
+
+    return SSE, new_centers
 
 # Specify K = the number of categorical values of y 
 # (The number of classifications; 10 labels)
@@ -148,10 +189,11 @@ def main(similarity_measure):
     # Pick random centers from the dataset to start the algorithm 
     initial_centers = rnd.choices(dataset, k=10)
     initial_centers = np.array(initial_centers)
-    #print(initial_centers.shape)
+    print("Shape of the initial centers:")
+    print(initial_centers.shape)
 
     # TODO: Keep track of iterations to answer question 3
-    #SSE_list =
+    #SSE_list = 
 
     return None
 
