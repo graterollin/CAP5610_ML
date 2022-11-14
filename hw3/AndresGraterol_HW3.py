81467e65ece228a838f735f8cbbf884713715b54
Andres Graterol <graterol.andres0@knights.ucf.edu>
2022-11-14 17:37:02 -0500

    Code runs without errors

--- hw3/AndresGraterol_HW3.py
+++ hw3/AndresGraterol_HW3.py
@@ -22,6 +22,10 @@ import random as rnd
 import matplotlib.pyplot as plt
 #from scipy.special import logsumexp
 
+# TODO: DECLUTTER THE CODE! REMOVE COMMENTS THAT WE KNOW FOR SURE ALREADY WORK!!!!!!!!!!!
+# TODO: Possibly refactor the code for the membership matrix to get away from one-hot encoding mess
+# TODO: SEE IF YOU CAN MANIPULATE THE DATA TO BE OF SHAPE (10000, 2 for example)
+
 def retrieve_data_from_files(data_path, label_path):
     # Init lists that will hold our data 
     dataset = []
@@ -37,7 +41,7 @@ def retrieve_data_from_files(data_path, label_path):
     # Shape of data should be (10000,784)
     dataset = np.array(dataset)
     #print(dataset)
-    print("Shape of the dataset")
+    #print("Shape of the dataset")
     print(dataset.shape)
 
     # Populate the labels 
@@ -48,7 +52,7 @@ def retrieve_data_from_files(data_path, label_path):
             labels.append(int_row)
 
     # Shape of labels should be (10000,)
-    print("Shape of the labels")
+    #print("Shape of the labels")
     labels = np.array(labels)
     labels = labels.squeeze()
     #print(labels)
@@ -100,7 +104,7 @@ def sse_distance_helper(center, point):
     sum_of_distances = sum(distance_array)
     final_distance = np.sqrt(sum_of_distances)
 
-    print("Distance for point: ", final_distance)
+    #print("Distance for point: ", final_distance)
     #print("Expecting 1 here: ")
     #print(final_distance.shape)
 
@@ -128,7 +132,7 @@ def compute_sse(centers, dataset, membership_matrix):
     sse_Total = sum(sse)
     sse_Total = np.array(sse_Total)
 
-    print("Final sse value:", sse_Total)
+    #print("Final sse value:", sse_Total)
     return sse_Total
 
 # This function computes the centroids and determines 
@@ -146,13 +150,14 @@ def kmeans_algorithm(centers, dataset, similarity_measure):
         sse_list.append(sse)
 
         print("Iteration:", iterations)
-        print("SSE List shape: ", len(sse_list))
+        #print("SSE List shape: ", len(sse_list))
         # return the final centers once they have converged (they do not change)
-        print("TIME TO COMPARE:")
-        print("New centers shape:", new_centers.shape)
-        print("Old centers shape: ", centers.shape)
+        #print("TIME TO COMPARE:")
+        #print("New centers shape:", new_centers.shape)
+        #print("Old centers shape: ", centers.shape)
 
         if (new_centers.all() == centers.all()):
+            print("Converged!")
             return centers, iterations, sse_list
         # Else, keep iterating
         else:
@@ -171,7 +176,7 @@ def compute_new_centroids(centers, dataset, similarity_type):
     # For every point in the dataset 
     for point in range(len(dataset)):
         # Create array to store similarity to each centroid
-        print("point # :", point)
+        #print("point # :", point)
 
         similarity_array = []
 
@@ -193,7 +198,7 @@ def compute_new_centroids(centers, dataset, similarity_type):
 
         # Determine the smallest number in the similarity array 
         # to determine cluster membership
-        print("Similarity array:", similarity_array)
+        #print("Similarity array:", similarity_array)
 
         min_sim = similarity_array[0]
         index = 0
@@ -209,7 +214,7 @@ def compute_new_centroids(centers, dataset, similarity_type):
         membership_array[cluster_member] = 1 
         membership_matrix.append(membership_array)
 
-    print("Final membership matrix:", membership_matrix)
+    #print("Final membership matrix:", membership_matrix)
     # TODO: Compute sse, create function
     # Compute sse for these centers and all the points 
     sse = compute_sse(centers, dataset, membership_matrix)
@@ -218,11 +223,12 @@ def compute_new_centroids(centers, dataset, similarity_type):
     # Get the total count in each cluster 
     cluster_totals = [sum(x) for x in zip(*membership_matrix)]
     cluster_totals = np.array(cluster_totals)
+
     # Expect shape: (10,)
-    print("Number per cluster shape:")
-    print(cluster_totals.shape)
-    print("Number per cluster:")
-    print(cluster_totals)
+    #print("Number per cluster shape:")
+    #print(cluster_totals.shape)
+    #print("Number per cluster:")
+    #print(cluster_totals)
 
     # 10 rows (one for each cluster)
     # 784 columns 
@@ -240,16 +246,17 @@ def compute_new_centroids(centers, dataset, similarity_type):
                 # Add to the cluster row    The point in row j  
                 sum_per_cluster[i] = np.add(sum_per_cluster[i], dataset[j])
 
-    print("Sum per cluster shape:", sum_per_cluster.shape)
+    #print("Sum per cluster shape:", sum_per_cluster.shape)
     new_centers = np.zeros((10, 784))
 
+    # TODO: DEBUG HERE, THE NEW CENTERS ARE NOT UPDATING!!!!
     # Divide each cluster row by the number of points in each cluster 
     for i in range(10):
         if (cluster_totals[i] != 0):
             new_centers[i] = [x / cluster_totals[i] for x in sum_per_cluster[i]] 
     
-    print("Shape of new centers:")
-    print(new_centers.shape)
+    #print("Shape of new centers:")
+    #print(new_centers.shape)
 
     return sse, new_centers
 
@@ -262,22 +269,25 @@ def main(similarity_measure):
 
     dataset, labels = retrieve_data_from_files(data_path, labels_path)
     
-    dataset = dataset[:20]
+    #dataset = dataset[:20]
 
     # Pick random centers from the dataset to start the algorithm 
     initial_centers = rnd.choices(dataset, k=10)
     initial_centers = np.array(initial_centers)
-    print("Shape of the initial centers:")
-    print(initial_centers.shape)
+    #print("Shape of the initial centers:")
+    #print(initial_centers.shape)
 
     # TODO: Keep track of iterations to answer question 3
     # TODO: Compute accuracies 
     centers, iterations, sse_list = kmeans_algorithm(initial_centers, dataset, similarity_measure)
     # Number of items in the sse_list will tell us how many times it ran before converging
     
-    #print("Number of iterations to converge: ", number_of_iterations)
+    print("ALGORITHM FINISHED")
+    print("Final Number of iterations:", iterations)
+    print("Final Shape of centers: ", centers.shape)
+    print("Length of sse_list:", len(sse_list))
     
-    x = np.linspace(0, iterations, iterations)
+    x = np.linspace(0, iterations+1, iterations+1)
     
     plt.scatter(x, sse_list)
     plt.show()
