0045ab83416a5031d61d6166c3ce9bd1a71421c0
Andres Graterol <graterol.andres0@knights.ucf.edu>
2022-11-19 12:55:32 -0500

    Initial HW3 Q2

---
+++ hw3/AndresGraterol_HW3_Q2.py
@@ -0,0 +1,11 @@
+# ----------------------------
+# Andres Graterol 
+# CAP5610 - Fall 22
+# 4031393
+# ----------------------------
+# Homework 3
+# QUESTION 2
+# ----------------------------
+
+import csv 
+import pandas as pd
