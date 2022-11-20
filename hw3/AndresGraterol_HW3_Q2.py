02ae48ffc1ec5995aeaa5e6a8d82f885eb50cbf9
Andres Graterol <graterol.andres0@knights.ucf.edu>
2022-11-20 00:21:59 -0500

    Created user and item based filterings

--- hw3/AndresGraterol_HW3_Q2.py
+++ hw3/AndresGraterol_HW3_Q2.py
@@ -6,10 +6,7 @@
 # Homework 3
 # QUESTION 2
 # ----------------------------
-
-import csv 
-import pandas as pd
-from surprise import SVD, Dataset, Reader
+from surprise import SVD, Dataset, Reader, KNNBasic
 from surprise.model_selection import cross_validate
 
 file_path = 'movies_data/ratings_small.csv'
@@ -22,6 +19,8 @@ reader = Reader(line_format='user item rating timestamp', sep=',', skip_lines=1)
 
 data = Dataset.load_from_file(file_path, reader)
 
+# Probabilistic Matrix Factorization
+# -----------------------------------------------------
 # Algorithm to use 
 # PMF is simply the SVD with no bias
 PMF = SVD(biased=False)
@@ -29,3 +28,25 @@ PMF = SVD(biased=False)
 # By default, the RMSE and MAE measures are computed
 # 5 K-Folds are used by default as well
 cross_validate(algo=PMF, data=data, verbose=True)
+
+# User-Based Collaborative Clustering 
+# -----------------------------------------------------
+user_based = {
+    "name": "msd",
+    "user_based": True,
+}
+
+userBased_filtering = KNNBasic(sim_options=user_based)
+
+cross_validate(algo=userBased_filtering, data=data, verbose=True)
+
+# Item-Based Collaborative Clustering 
+# -----------------------------------------------------
+item_based = {
+    "name": "msd",
+    "user_based": False,
+}
+
+itemBased_filtering = KNNBasic(sim_options=item_based)
+
+cross_validate(algo=itemBased_filtering, data=data, verbose=True)
