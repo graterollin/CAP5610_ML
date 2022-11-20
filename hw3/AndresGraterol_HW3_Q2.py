21f3da7c8dad702214979040f2e63f1b37dd88dc
Andres Graterol <graterol.andres0@knights.ucf.edu>
2022-11-19 22:41:28 -0500

    PMF algorithm done

--- hw3/AndresGraterol_HW3_Q2.py
+++ hw3/AndresGraterol_HW3_Q2.py
@@ -11,3 +11,21 @@ import csv
 import pandas as pd
 from surprise import SVD, Dataset, Reader
 from surprise.model_selection import cross_validate
+
+file_path = 'movies_data/ratings_small.csv'
+
+# TODO: Check this separator 
+#       Possibly use header = none 
+#       Check stack overflow post 
+#       Use double quotes?
+reader = Reader(line_format='user item rating timestamp', sep=',', skip_lines=1)
+
+data = Dataset.load_from_file(file_path, reader)
+
+# Algorithm to use 
+# PMF is simply the SVD with no bias
+PMF = SVD(biased=False)
+
+# By default, the RMSE and MAE measures are computed
+# 5 K-Folds are used by default as well
+cross_validate(algo=PMF, data=data, verbose=True)
